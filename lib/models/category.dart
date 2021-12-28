8e3184e1c62d212b58cd50f986ed2f67ce9176f8
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2021-12-29 00:26:54 +0400

    first commit ��

---
+++ lib/models/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Categories {
+  String id;
+  String title;
+  Color color;
+
+  Categories({required this.id, required this.title, required this.color});
+}
